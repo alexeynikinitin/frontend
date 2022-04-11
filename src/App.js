2df79d3305e19e4d48311d7ace4407d3a2c3f839
alexeynikinitin <alexeynikinitin@yandex.by>
2022-04-11 21:25:12 +0300

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([])
+  const getUsers = () => {
+    axios.get("http://localhost:7542/users")
+      .then((res) => {
+        setUsers(res.data)
+      })
+  }
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  let onClick = () => {
+    axios.post("http://localhost:7542/users")
+      .then((res) => {
+        getUsers()
+      })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div>
+        <button onClick={onClick}>Create</button>
+      </div>
+      { users.map((u, i) => <div key={`u.name-${i}`}>{u.name}</div>) }
     </div>
   );
 }
