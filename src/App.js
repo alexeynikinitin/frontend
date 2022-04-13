e9a38bc5e809e503b957785277f01ed38d6ddec5
alexeynikinitin <alexeynikinitin@yandex.by>
2022-04-13 21:12:51 +0300

    Lesson 5

--- src/App.js
+++ src/App.js
@@ -6,30 +6,52 @@ function App() {
   const userNameRef = useRef(null)
 
   const [users, setUsers] = useState([])
+
   const getUsers = () => {
-    axios.get("http://localhost:7542/users")
+    axios.get(`http://localhost:7542/users` + window.location.search)
       .then((res) => {
         setUsers(res.data)
       })
   }
-  useEffect(() => {
-    getUsers();
-  }, [])
 
-  let onClick = () => {
+  const createUser = () => {
     axios.post("http://localhost:7542/users", {id: 4, name: userNameRef.current.value})
       .then((res) => {
         getUsers()
       })
   };
 
+  const deleteUser = (id) => {
+    axios.delete(`http://localhost:7542/users/${id}`)
+      .then((res) => {
+        getUsers()
+      })
+  }
+
+  const updateUser = (id, name) => {
+    axios.put(`http://localhost:7542/users`, {id, name})
+      .then((res) => {
+        getUsers()
+      })
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
   return (
-    <div >
+    <div>
       <input ref={userNameRef}/>
       <div>
-        <button onClick={onClick}>Create</button>
+        <button onClick={createUser}>Create</button>
       </div>
-      { users.map((u, i) => <div key={`u.name-${i}`}>{u.name}</div>) }
+      {users.map((u) =>
+        <div
+          key={u._id}
+        >
+          <input defaultValue={u.name} onBlur={(e) => {updateUser(u._id, e.currentTarget.value)}}/>
+          <button onClick={() => deleteUser(u._id)}>X</button>
+        </div>)}
     </div>
   );
 }
